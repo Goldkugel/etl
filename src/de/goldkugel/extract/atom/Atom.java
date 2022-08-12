a03d980721f22d2eb641a5f013d2b6021bd96a7d
pallaoro <peter.pallaoro@tum.de>
2022-08-12 14:26:24 +0200

    Added lombok to the project.
    Added lombock to the project and created the code for the class Atom.

--- src/de/goldkugel/extract/atom/Atom.java
+++ src/de/goldkugel/extract/atom/Atom.java
@@ -5,23 +5,55 @@ package de.goldkugel.extract.atom;
 
 import java.io.Serializable;
 
+import lombok.Data;
+import lombok.NonNull;
+
 /**
  * 
  * @author Peter Pallaoro
  *
  */
+@Data
 public class Atom implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -1574074894772892645L;
+	
+	/**
+	 * 
+	 */
+	@NonNull
+	private String name = null;
+	
+	/**
+	 * 
+	 */
+	private String content = null;
 
 	/**
 	 * 
 	 */
 	public Atom() {
-
+		
 	}
-
+	
+	/**
+	 * 
+	 * @param name
+	 */
+	public Atom(String name) {
+		this.setName(name);
+	}
+	
+	/**
+	 * 
+	 * @param name
+	 * @param content
+	 */
+	public Atom(String name, String content) {
+		this(name);
+		this.setContent(content);
+	}
 }
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author pallaoro
- *
- */
-module extract {
-}
