687789f02efcffbcbe73e2cdadde123a03058091
pallaoro <peter.pallaoro@tum.de>
2022-08-12 14:06:30 +0200

    Javadoc was created.
    The javadoc was created. Might need updates in future.

---
+++ doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"extract"}];updateSearchResults();
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"de.goldkugel.extract","l":"Extract"}];updateSearchResults();
