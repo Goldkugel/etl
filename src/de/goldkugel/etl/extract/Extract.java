e8270b48ccd53b57861d5ec8bf449bdfaa4eaf34
pallaoro <peter.pallaoro@tum.de>
2022-11-10 11:53:23 +0100

    Added some comment fields.

--- src/de/goldkugel/etl/extract/Extract.java
+++ src/de/goldkugel/etl/extract/Extract.java
@@ -14,8 +14,19 @@ import de.goldkugel.etl.Configurable;
  */
 public interface Extract  extends Configurable  {
 	
+	/**
+	 * 
+	 * @param l
+	 * @return
+	 */
 	public int read(List<Object> l);
 	
+	/**
+	 * 
+	 * @param l
+	 * @param amount
+	 * @return
+	 */
 	public int read(List<Object> l, int amount);
 	
 }
